3755c31e013fe2e9f81981c78adce8a5c006a1d1
maxyao <maxyao@lexin.com>
2019-12-27 18:33:28 +0800

    dsfsd

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,15 @@
 <script>
-	import { onMount } from 'svelte';
 	
-	const list  = []
-	for(let i = 0; i < 50000; i++) {
-		list.push(i);
-	}
-	const beginTime = +new Date();
-	let name = 'world';
-	let data = new Array(1000)
+	let name = 'svelte';
+	let data = new Array(500)
 	function click(index){
 		return ()=>{
 			data[index] = 'test'
 		}
 	}
-	onMount(()=>{
-		const endTime = +new Date();
-	  console.log((endTime - beginTime) / 1000,1);
-	})
 </script>
 
+<div>数据量: { data.length }</div>
 {#each data as d, i}
 <h1 on:click={click(i)}>
 	<span>
